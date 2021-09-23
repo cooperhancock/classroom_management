2a1fbead79ff118d03ac862968176f4b82b193fb
Ritik Mishra <ritik.mishra314@gmail.com>
2021-09-23 18:51:29 -0500

    add minimum stuff for backend and frontend

---
+++ backend/src/app.ts
@@ -0,0 +1,33 @@
+import Koa from 'koa';
+const app = new Koa();
+
+// logger
+
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.get('X-Response-Time');
+  console.log(`${ctx.method} ${ctx.url} - ${rt}`);
+});
+
+// x-response-time
+
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// response
+
+app.use(async ctx => {
+  ctx.body = 'Hello World';
+});
+
+const main = () => {
+    const PORT = 3000;
+    console.log(`Listening on port ${PORT}`);
+    app.listen(PORT);
+}
+
+main()
