f5ba6a656248e7a9dce42f123880f0b01cb62812
Ritik Mishra <ritik.mishra314@gmail.com>
2021-09-23 19:19:18 -0500

    set up eslint, prettier

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,20 +1,25 @@
 module.exports = {
     "ignorePatterns": [".eslintrc.js"],
     "env": {
-        "es2021": true,
-        "node": true
+        "browser": true,
+        "es2021": true
     },
     "extends": [
         "eslint:recommended",
+        "plugin:react/recommended",
         "plugin:@typescript-eslint/recommended",
         "plugin:prettier/recommended"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
         "ecmaVersion": 12,
         "sourceType": "module"
     },
     "plugins": [
+        "react",
         "@typescript-eslint"
     ],
     "rules": {
